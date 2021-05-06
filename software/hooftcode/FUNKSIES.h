82ebac13e4f7bafea28e8adf127f62186e2762a4
DEBIE-LAUREN <75269693+DEBIE-LAUREN@users.noreply.github.com>
2021-05-06 11:34:40 +0200

    Create FUNKSIES.h
    eerste uplowd Lauren

---
+++ software/hooftcode/FUNKSIES.h
@@ -0,0 +1,23 @@
+/*  regels funksie.h:
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *  -   WANEER TOEVOEGEN FUNKSIE ALTIJT:
+ *      1) VERWIJDER DIT BESTANT FUNKSIES.H UIT PROJECT
+ *      2) GA NAAR GITUP EN KOPJEER FUNKSIES.H NAAR JE PROJECT
+ *      3) VOEG FUNKSIE TOE
+ *      4) COPJEER FUNKSIES.H NAAR GITHUP EN UPLOWD DA
+ *!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * 
+ * 
+ * 
+ *  -   enkel funksies inzetten die voledig werken (maaken van funksies gebeurt 
+ *      in testfunksies_debie.h of testfunksies_toon.h
+ *  -   beschrijf de stappen van uw funksie en houw het ordelijk
+ *  -   waneer een funksie af is zet men de naam van de funksie met korte 
+ *      beschrijving in de funksie lookuptabel
+ *  -   max lente regel is tot de rode lijn  
+ */
+
+/*  funksie lookup tabel: 
+ */
+
+
