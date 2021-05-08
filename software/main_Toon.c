b9a044cee408fc5e9b7123bcdf46ffe4cb8bdaaa
Toon-Jansen <r0781183@student.thomasmore.be>
2021-05-08 20:54:58 +0200

    I2C werking test

--- software/main_Toon.c
+++ software/main_Toon.c
@@ -12,22 +12,69 @@
 #include "hooftcode/header/Toon_header.h"
 #include "hooftcode/header/configerasenbits_pic.h"
 
+void I2C_Initialize(const unsigned long feq_K) //Begin IIC as master
+{
+  TRISC3 = 1;  TRISC4 = 1;  //Set SDA and SCL pins as input pins
 
+  SSPCON  = 0b00101000;    //pg84/234
+  SSPCON2 = 0b00000000;    //pg85/234
 
+  SSPADD = (_XTAL_FREQ/(4*feq_K*100))-1; //Setting Clock Speed pg99/234
+  SSPSTAT = 0b00000000;    //pg83/234
+}
 
+void I2C_Hold()
+{
+    while (   (SSPCON2 & 0b00011111)    ||    (SSPSTAT & 0b00000100)   ) ; //check the this on registers to make sure the IIC is not in progress
+}
+
+void I2C_Begin()
+{
+  I2C_Hold();  //Hold the program is I2C is busy 
+  SEN = 1;     //Begin IIC pg85/234
+}
+void I2C_End()
+{
+  I2C_Hold(); //Hold the program is I2C is busy 
+  PEN = 1;    //End IIC pg85/234
+}
+
+void I2C_Write(unsigned data)
+{
+  I2C_Hold(); //Hold the program is I2C is busy
+  SSPBUF = data;         //pg82/234
+}
+
+unsigned short I2C_Read(unsigned short ack)
+{
+  unsigned short incoming;
+  I2C_Hold();
+  RCEN = 1;
+
+  I2C_Hold();
+  incoming = SSPBUF;      //get the data saved in SSPBUF
+
+  I2C_Hold();
+  ACKDT = (ack)?0:1;    //check if ack bit received 
+  ACKEN = 1;          //pg 85/234
+
+  return incoming;
+}
 
 void main(void) 
 {
-    init_clk(1);
-    TRISB0 = 0;
-    while(1)
-    {
-        __delay_ms(100);
-        RB0 = 1;
-        
-        __delay_ms(100);
-        RB0 = 0;
-        
-    }
-    return;
+    I2C_Initialize(100); //Initialize I2C Master with 100KHz clock
+
+while(1)
+  {
+   I2C_Begin();       
+   I2C_Write(0xD0); 
+   I2C_Write(0x88); 
+   I2C_Write(0xFF); 
+   I2C_End();
+   
+   __delay_ms(1000);
+
 }
+}
+
