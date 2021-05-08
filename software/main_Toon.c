778103d696b35f7935d14f2217ce0b772bf2ded0
DEBIE-LAUREN <75269693+DEBIE-LAUREN@users.noreply.github.com>
2021-05-08 20:53:09 +0200

    verwijder

--- software/main_Toon.c
+++ software/main_Toon.c
@@ -5,7 +5,7 @@
  * Created on 8 mei 2021, 12:16
  */
 
-#define _XTAL_FREQ 250000
+#define _XTAL_FREQ 8000000
 #include <xc.h>
 #include "hooftcode/header/Debie_header.h"
 #include "hooftcode/header/FUNKSIES_heade.h"
@@ -14,20 +14,70 @@
 
 
 
+/*Very Important - change _XTAL_FREQ to match target clock 
+  
+  This program initialised UART and port for LEDs and waits for a character to be received on the serial port.
+	It displays character on LEDS (active low) and then returns it on serial port.
+*/
 
 
-void main(void) 
+// Function Prototypes
+char UART_Init(const long int baudrate);
+char UART_Data_Ready();
+char UART_Read();
+void UART_Write(char data);
+
+led_display(unsigned char value);
+
+// Main Function
+void main()
 {
-    init_clk(1);
-    TRISB0 = 0;
-    while(1)
-    {
-        __delay_ms(100);
-        RB0 = 1;
-        
-        __delay_ms(100);
-        RB0 = 1;
-        
-    }
-    return;
+    unsigned char 8;
+    
+   TRISD = 0x00;  // PORTD as outputs for LEDS
+   UART_Init(9600);
+   
+   while(1)
+   {
+                  // display on LEDs
+       __delay_ms(100);
+       UART_Write(value);            // write it back 
+   }
+}
+
+ // functions
+
+led_display(unsigned char value){
+
+    PORTD = ~value;
+}
+
+char UART_Init(const long int baudrate){
+	unsigned int x;
+	x = (_XTAL_FREQ - baudrate*16)/(baudrate*16);
+	BRGH = 1;
+	SPBRG = x;
+	  SYNC = 0;
+	  SPEN = 1;
+          TRISC7 = 1;
+          TRISC6 = 1;
+          CREN = 1;
+          TXEN = 1;
+	  return 1;	
+}
+
+
+char UART_Data_Ready(){
+   return RCIF;
+}
+
+char UART_Read(){
+ 
+  while(!RCIF);
+  return RCREG;
+}
+
+void UART_Write(char data){
+  while(!TRMT);
+  TXREG = data;
 }
